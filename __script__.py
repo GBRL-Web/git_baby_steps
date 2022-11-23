5aeb025ceb012bdccda733bfc702a988832b3f3f
GBRL-Web <gabi.titi.dnd@gmail.com>
2022-11-23 14:24:01 +0100

    fixed merge

--- __script__.py
+++ __script__.py
@@ -2,7 +2,5 @@ monPremierCommit = 1
 maPremiereBranche = 0
 maPremiereFusion = 0
 maPremiereErreur = 1
-maPremiereFusion = 1
-maPremiereErreur = 0
 jeVaisMalNommerMonProchaonCommit = 1
 unicormeMode = 1
